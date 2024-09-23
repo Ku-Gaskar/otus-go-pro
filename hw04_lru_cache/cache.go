9eedf2d7461b8fd43923b56d41d8bb012ddda972
Kusmarov Alexandr <kusmarov_a@mail.ru>
2024-09-23 18:24:28 +0300

    hw04_lru_cache - homework_4 after review fix_3

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -2,8 +2,6 @@ package hw04lrucache
 
 type Key string
 
-var cache = make(map[*ListItem]Key)
-
 type Cache interface {
 	Set(key Key, value interface{}) bool
 	Get(key Key) (interface{}, bool)
@@ -11,16 +9,18 @@ type Cache interface {
 }
 
 type lruCache struct {
-	capacity int
-	queue    List
-	items    map[Key]*ListItem
+	capacity  int
+	queue     List
+	items     map[Key]*ListItem
+	cacheKeys map[*ListItem]Key
 }
 
 func NewCache(capacity int) Cache {
 	return &lruCache{
-		capacity: capacity,
-		queue:    NewList(),
-		items:    make(map[Key]*ListItem, capacity),
+		capacity:  capacity,
+		queue:     NewList(),
+		items:     make(map[Key]*ListItem, capacity),
+		cacheKeys: make(map[*ListItem]Key),
 	}
 }
 
@@ -32,22 +32,23 @@ func (c *lruCache) Set(key Key, value interface{}) bool {
 	}
 
 	if c.queue.Len() >= c.capacity { // Елемента нет в словаре и длинна очереди больше вместимости кэша
-		last := c.queue.Back()       // Получаем последний элемент очереди
-		c.queue.Remove(last)         // Удаляем последний элемент из очереди
-		delete(c.items, cache[last]) // Удаляем последний элемент из мапы
-		delete(cache, last)
+		last := c.queue.Back()             // Получаем последний элемент очереди
+		c.queue.Remove(last)               // Удаляем последний элемент из очереди
+		delete(c.items, c.cacheKeys[last]) // Удаляем последний элемент из мапы
+		delete(c.cacheKeys, last)
 	}
 
 	item := c.queue.PushFront(key) // Перемещаем в начало очереди
 	item.Value = value
-	cache[item] = key   // Обновляем значение
-	c.items[key] = item // Сохраняем в мапу
+	c.cacheKeys[item] = key // Обновляем значение
+	c.items[key] = item     // Сохраняем в мапу
 	return false
 }
 
 func (c *lruCache) Get(key Key) (interface{}, bool) {
 	if item, ok := c.items[key]; ok { // Элемент присутсвует в словаре
 		c.queue.MoveToFront(item) // Перемещаем элемент в начало очереди
+		c.queue.Back()
 		return item.Value, true
 	}
 	return nil, false
@@ -56,5 +57,5 @@ func (c *lruCache) Get(key Key) (interface{}, bool) {
 func (c *lruCache) Clear() {
 	c.queue = NewList()
 	c.items = make(map[Key]*ListItem, c.capacity)
-	cache = make(map[*ListItem]Key)
+	c.cacheKeys = make(map[*ListItem]Key)
 }
