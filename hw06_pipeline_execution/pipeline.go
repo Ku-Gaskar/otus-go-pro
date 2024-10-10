1ae5339cad014ad252e6af9b5ed4e56ae96dc458
user <ku@aver-s.com.ua>
2024-10-10 16:09:44 +0300

    ok

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,45 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, stage := range stages {
+		in = stage(forwarder(in, done))
+	}
+	return in
+}
+
+// forwarder - передает данные между стейджами, учитывая сигнал остановки
+func forwarder(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer func() {
+			close(out) // Закрываем выходной канал
+			// Читаем остаточные данные из канала in, чтобы избежать блокировок
+			for range in {
+			}
+		}()
+
+		for {
+			select {
+			case <-done:
+				// Если пришел сигнал завершения, выходим из горутины
+				return
+			case v, ok := <-in:
+				if !ok {
+					// Если входной канал закрыт, завершаем работу
+					return
+				}
+				select {
+				case <-done:
+					// Если пришел сигнал завершения, выходим из горутины
+					return
+
+				// Отправляем результат в выходной канал, если нет сигнала завершения
+				case out <- v:
+					break
+				}
+			}
+		}
+	}()
+
+	return out
 }
